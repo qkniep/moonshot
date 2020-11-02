e6896e0ec2a9ed87220189c3d9055be2fc378da6
Quentin Kniep <hello@quentinkniep.com>
2020-11-02 16:55:59 +0100

    ✨ basic rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+// Copyright (C) 2020 qkniep <qkniep@qkmac>
+// Distributed under terms of the MIT license.
+
+use amethyst::{
+    core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    ui::{RenderUi, UiBundle},
+    utils::application_root_dir,
+};
+use amethyst_rendy::palette::Srgba;
+
+mod state;
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+    let key_bindings_path = app_root.join("config").join("input.ron");
+
+    let (r, g, b, a) = Srgba::new(0. / 255.,  26. / 255., 68. / 255., 1.)
+        .into_linear()
+        .into_components();
+
+    let game_data = GameDataBuilder::default()
+    .with_bundle(TransformBundle::new())?
+    .with_bundle(
+        InputBundle::<StringBindings>::new().with_bindings_from_file(&key_bindings_path)?,
+    )?
+    .with_bundle(UiBundle::<StringBindings>::new())?
+    .with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                    .with_clear([r, g, b, a]),
+            )
+            .with_plugin(RenderUi::default())
+            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+
+    let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, state::GameplayState, game_data)?;
+    game.run();
+
+    Ok(())
 }
---
+++ src/state.rs
@@ -0,0 +1,191 @@
+// Copyright (C) 2020 qkniep <qkniep@qkmac>
+// Distributed under terms of the MIT license.
+
+use amethyst::{
+    assets::{AssetStorage, Loader},
+    core::transform::Transform,
+    input::{get_key, is_close_requested, is_key_down, VirtualKeyCode},
+    prelude::*,
+    renderer::{
+        Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture, Transparent,
+    },
+    ui::{
+        Anchor, FontHandle, LineMode, Stretch, TtfFormat, UiButtonBuilder, UiImage, UiText,
+        UiTransform,
+    },
+    window::ScreenDimensions,
+};
+use amethyst_rendy::palette::Srgba;
+
+pub struct GameplayState;
+
+impl SimpleState for GameplayState {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        // Get the screen dimensions so we can initialize the camera and
+        // place our sprites correctly later. We'll clone this since we'll
+        // pass the world mutably to the following functions.
+        let dimensions = (*world.read_resource::<ScreenDimensions>()).clone();
+
+        // Place the camera
+        init_camera(world, &dimensions);
+
+        // Load our sprites and display them
+        let sprites = load_sprites(world);
+        init_sprites(world, &sprites, &dimensions);
+
+        create_ui_example(world);
+    }
+
+    fn fixed_update(&mut self, _data: StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        SimpleTrans::None
+    }
+}
+
+/// Creates a camera entity in the `world`.
+///
+/// The `dimensions` are used to center the camera in the middle
+/// of the screen, as well as make it cover the entire screen.
+fn init_camera(world: &mut World, dimensions: &ScreenDimensions) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(dimensions.width() * 0.5, dimensions.height() * 0.5, 1.);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(dimensions.width(), dimensions.height()))
+        .with(transform)
+        .build();
+}
+
+/// Loads and splits the `planets.png` image asset into 3 sprites,
+/// which will then be assigned to entities for rendering them.
+///
+/// The provided `world` is used to retrieve the resource loader.
+fn load_sprites(world: &mut World) -> Vec<SpriteRender> {
+    // Load the texture for our sprites. We'll later need to
+    // add a handle to this texture to our `SpriteRender`s, so
+    // we need to keep a reference to it.
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "sprites/planets.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    // Load the spritesheet definition file, which contains metadata on our
+    // spritesheet texture.
+    let sheet_handle = {
+        let loader = world.read_resource::<Loader>();
+        let sheet_storage = world.read_resource::<AssetStorage<SpriteSheet>>();
+        loader.load(
+            "sprites/planets.ron",
+            SpriteSheetFormat(texture_handle),
+            (),
+            &sheet_storage,
+        )
+    };
+
+    // Create our sprite renders. Each will have a handle to the texture
+    // that it renders from. The handle is safe to clone, since it just
+    // references the asset.
+    (0..3)
+        .map(|i| SpriteRender {
+            sprite_sheet: sheet_handle.clone(),
+            sprite_number: i,
+        })
+        .collect()
+}
+
+/// Creates an entity in the `world` for each of the provided `sprites`.
+/// They are individually placed around the center of the screen.
+fn init_sprites(world: &mut World, sprites: &[SpriteRender], dimensions: &ScreenDimensions) {
+    let positions = [(350., 250.), (1200., 900.), (2100., 600.)];
+
+    for (i, sprite) in sprites.iter().enumerate() {
+        // Center our sprites around the center of the window
+        let (x, y) = positions[i];
+        //let x = (i as f32 - 1.) * 100. + dimensions.width() * 0.5;
+        //let y = (i as f32 - 1.) * 100. + dimensions.height() * 0.5;
+        let mut transform = Transform::default();
+        transform.set_translation_xyz(x, y, 0.);
+
+        // Create an entity for each sprite and attach the `SpriteRender` as
+        // well as the transform. If you want to add behaviour to your sprites,
+        // you'll want to add a custom `Component` that will identify them, and a
+        // `System` that will iterate over them. See https://book.amethyst.rs/stable/concepts/system.html
+        world
+            .create_entity()
+            .with(sprite.clone())
+            .with(transform)
+            .with(Transparent)
+            .build();
+    }
+}
+
+/// Creates a simple UI background and a UI text label
+/// This is the pure code only way to create UI with amethyst.
+pub fn create_ui_example(world: &mut World) {
+    let (r, g, b, a) = Srgba::new(37. / 255., 205. / 255., 227. / 255., 0.8).into_linear().into_components();
+    // this creates the simple gray background UI element.
+    let ui_background = world
+        .create_entity()
+        .with(UiImage::SolidColor([r, g, b, a]))
+        .with(UiTransform::new(
+            "".to_string(),
+            Anchor::TopLeft,
+            Anchor::TopLeft,
+            30.0,
+            -30.,
+            0.,
+            250.,
+            50.,
+        ))
+        .with(Transparent)
+        .build();
+
+    // This simply loads a font from the asset folder and puts it in the world as a resource,
+    // we also get a ref to the font that we then can pass to the text label we crate later.
+    let font: FontHandle = world.read_resource::<Loader>().load(
+        "fonts/Nunito-SemiBold.ttf",
+        TtfFormat,
+        (),
+        &world.read_resource(),
+    );
+
+    // This creates the actual label and places it on the screen.
+    // Take note of the z position given, this ensures the label gets rendered above the background UI element.
+    world
+        .create_entity()
+        .with(UiTransform::new(
+            "".to_string(),
+            Anchor::TopLeft,
+            Anchor::TopLeft,
+            40.0,
+            -31.,
+            1.,
+            200.,
+            50.,
+        ))
+        .with(UiText::new(
+            font,
+            "0".to_string(),
+            [1., 1., 1., 1.],
+            48.,
+            LineMode::Single,
+            Anchor::TopLeft,
+        ))
+        .build();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {}
+}
