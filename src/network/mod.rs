b587edc3cf6ebd3ee72bfa3a382d609a4a70ef34
Quentin Kniep <hello@quentinkniep.com>
2020-11-16 12:12:48 +0100

    🚧 start cleaning up messy network code

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -48,16 +48,13 @@ impl Plugin for NetworkPlugin {
     fn build(&self, app: &mut AppBuilder) {
         let stream = TcpStream::connect("127.0.0.1:7777").unwrap();
         stream.set_nonblocking(true).unwrap();
-        app.add_resource(NetworkResource {
-            stream,
-            recv_buffer: vec![0; 65536],
-        })
-        .add_resource(Events::<NetworkSimulationEvent>::default())
-        .add_resource(Transport::default())
-        .add_resource(NetworkSimulationTime::default())
-        .add_system(update_simulation_time.system())
-        .add_system(send_messages.system())
-        .add_system(handle_messages.system());
+        app.add_resource(stream)
+            .add_resource(Events::<NetworkSimulationEvent>::default())
+            .add_resource(Transport::default())
+            .add_resource(NetworkSimulationTime::default())
+            .add_system(update_simulation_time.system())
+            .add_system(send_messages.system())
+            .add_system(handle_messages.system());
     }
 }
 
@@ -88,66 +85,27 @@ impl Transport {
     }
 }
 
-fn send_messages(mut transport: ResMut<Transport>, mut net: ResMut<NetworkResource>) {
+fn send_messages(mut transport: ResMut<Transport>, mut stream: ResMut<TcpStream>) {
     let messages = transport.drain_messages();
     for message in messages {
-        net.stream
+        stream
             .write_all(&[(message.length / 256) as u8, (message.length % 256) as u8])
             .expect("Failed to send length of network message~");
-        if let Err(e) = net.stream.write_all(&message.payload) {
+        if let Err(e) = stream.write_all(&message.payload) {
             eprintln!("Failed to send network message: {}", e);
         }
     }
 }
 
-struct NetworkResource {
-    stream: TcpStream,
-    recv_buffer: Vec<u8>,
-}
-
-impl NetworkResource {
-    fn read(&mut self) -> Vec<ServerTurn> {
-        if let Ok(msg) = bincode::deserialize_from(&mut self.stream) {
-            vec![msg]
-        } else {
-            Vec::new()
-        }
-        /*match self.stream.read(&mut self.recv_buffer) {
-            Ok(0) => {
-                eprintln!("Connection closed!");
-                Vec::new()
-            }
-            Ok(_) => {
-                let mut result = Vec::new();
-                while self.buf_fill >= 2 {
-                    let size = self.recv_buffer[0] as usize * 256 + self.recv_buffer[1] as usize;
-                    if self.buf_fill - 2 < size {
-                        break;
-                    }
-
-                    result.push(self.recv_buffer[2..2 + size].into());
-                    self.buf_fill -= 2 + size;
-                }
-                result
-            }
-            Err(e) => match e.kind() {
-                std::io::ErrorKind::ConnectionReset => panic!("Connection reset by other party!"),
-                std::io::ErrorKind::WouldBlock => Vec::new(),
-                _ => panic!("Failed to receive bytes on TcpStream!"),
-            },
-        }*/
-    }
-}
-
 fn handle_messages(
-    mut net: ResMut<NetworkResource>,
+    mut stream: ResMut<TcpStream>,
     mut event_channel: ResMut<Events<NetworkSimulationEvent>>,
 ) {
-    let peer_addr = net.stream.peer_addr().unwrap();
+    let peer_addr = stream.peer_addr().unwrap();
 
-    for turn in net.read() {
-        //let turn = bincode::deserialize::<ServerTurn>(&msg_payload).unwrap();
+    if let Ok(turn) = bincode::deserialize_from(&mut *stream) {
         println!("Received msg: {:?}", turn);
+        let x: ServerTurn = turn;
         //event_channel.send(NetworkSimulationEvent::Message(peer_addr, msg_payload));
     }
 }
